882bb606e00db532ee50b91e742978e407853a33
fkimani <mainafk@gmail.com>
2022-01-12 22:50:26 -0500

    read file operations

--- app.py
+++ app.py
@@ -1,7 +1,33 @@
-import requests
 
-url = "https://adventofcode.com/2021/day/1/input"
+# returns file object
+f = open('data.txt', 'r')
+#store the data from file in array
+mydata = []
 
-page = requests.get(URL)
+for line in f:
+    mydata.append(int(line)) #convert to int to do number comparissons
+    # print(int(line))
+first = mydata[0] 
+last = mydata[len(mydata)-1]
+# print(type(first))
+print(f'first item {first}') # check
+print(f'last item {last}') # check
+print(f'len data: {len(mydata)}') # check
+print(len(mydata)==2000) # check
+g = 0
+l = 0
+for d in mydata:
+    if d == first:
+        print(f'{d} skip first item {first}')
+    if d > first:
+        # print(f'{d} greater than {first}')
+        g+=1
+    else:
+        # print(f'{d} lesser than {first}')  
+        l+=1
+    first = d
 
-print(page.text)
+print(f'Greater than count: {g}')
+# print(f' Lesser than count: {l}')
+# print(g+l)
+f.close()
