75576c5b12aef796daf287435911d846b1eb57c2
fkimani <mainafk@gmail.com>
2022-01-16 00:29:27 -0500

    late night fails

---
+++ day3/day3.py
@@ -0,0 +1,48 @@
+"""
+Use the binary numbers in your diagnostic report to 
+calculate the gamma rate and epsilon rate, then multiply 
+them together. What is the power consumption of the submarine? 
+(Be sure to represent your answer in decimal, not binary.) 
+"""
+
+# vars
+# gamma_rate = 0
+# epsilon_rate = 0
+
+# file
+myfile = "day3/data-sample.txt"
+
+# returns file object
+f = open(myfile, 'r')
+
+for line in f:
+    champ = 0
+    gamma_rate = 0
+    epsilon_rate = 0
+    ll = len(line)
+    i = 0
+    if(ll>0):
+        print(line[i])
+        
+    for l in line:
+        if(l=='0'):
+            print("\t ",l)
+            epsilon_rate += 1
+        if(l=='1'):
+            print("\t ",l)
+            gamma_rate += 1
+    ll -= 1
+    i += 1
+    print("------")
+
+    # # print(f'gamma Rate={gamma_rate}; epsilon Rate={epsilon_rate} ')
+    # if(gamma_rate>epsilon_rate):
+    #     print(f"\tgamma rate is {gamma_rate} (greater)")
+    #     champ = 1
+    # if(gamma_rate<epsilon_rate):
+    #     print(f"\tepsilon rate is {epsilon_rate} (greater)")
+    #     champ = 0
+    # # if(gamma_rate==epsilon_rate): #may not happen
+    # #     print(f"gamma and epsilon rate equal({gamma_rate==epsilon_rate})")
+    # print(f"champ: {champ}")
+    # print("---") 
